b595358e87a2d94be91ccc9fc725a6eed37cf014
abraham <abrahamgitonga98@gmail.com>
2022-10-04 20:49:53 +0300

    add initiall js file

---
+++ app.js
@@ -0,0 +1,19 @@
+//how to add animation?
+function animate(elem,style,unit,from,to,time) {
+    if( !elem) return;
+    var start = new Date().getTime(),
+        timer = setInterval(function() {
+            var step = Math.min(1,(new Date().getTime()-start)/time);
+            elem.style[style] = (from+step*(to-from))+unit;
+            if( step == 1) clearInterval(timer);
+        },25);
+    elem.style[style] = from+unit;
+}
+
+
+animate(
+    document.getElementById('challengeOneImageJavascript'),
+    "left","px",0,200,1000
+);
+
+
